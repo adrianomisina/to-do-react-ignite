2f8b137bdb3880b3e8425995e28218648ca5dacb
Adriano Misina <adriano.misina@gmail.com>
2023-06-21 09:58:09 -0300

    estilização da tela tasklist sem tasks

--- src/App.tsx
+++ src/App.tsx
@@ -10,26 +10,26 @@ export interface TaskType {
 
 const App = () => {
   const [tasks, setTasks] =useState<TaskType[]>([
-    // {
-    //   id: 1,
-    //   title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-    //   isCompleted: false
-    // },
-    // {
-    //   id: 2,
-    //   title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-    //   isCompleted: false
-    // },
-    // {
-    //   id: 3,
-    //   title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-    //   isCompleted: false
-    // },
-    // {
-    //   id: 4,
-    //   title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-    //   isCompleted: false
-    // }
+    {
+      id: 1,
+      title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+      isCompleted: false
+    },
+    {
+      id: 2,
+      title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+      isCompleted: false
+    },
+    {
+      id: 3,
+      title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+      isCompleted: false
+    },
+    {
+      id: 4,
+      title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+      isCompleted: false
+    }
   ])
 
   const handleCreateTask = (newTaskTitle: string) => {
