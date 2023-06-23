5600f77866f7415d29560bfe9be3fe8e3edfe6dd
Adriano Misina <adriano.misina@gmail.com>
2023-06-23 13:57:22 -0300

    clean list

--- src/App.tsx
+++ src/App.tsx
@@ -9,28 +9,7 @@ export interface TaskType {
 }
 
 const App = () => {
-  const [tasks, setTasks] =useState<TaskType[]>([
-    {
-      id: 1,
-      title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-      isCompleted: false
-    },
-    {
-      id: 2,
-      title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-      isCompleted: false
-    },
-    {
-      id: 3,
-      title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-      isCompleted: false
-    },
-    {
-      id: 4,
-      title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-      isCompleted: false
-    }
-  ])
+  const [tasks, setTasks] =useState<TaskType[]>([])
 
   const handleCreateTask = (newTaskTitle: string) => {
     const newTask: TaskType = {
